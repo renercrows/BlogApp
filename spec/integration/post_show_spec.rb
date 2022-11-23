de06132b3bcdb51719ac778aa40a650803241856
renercrows <ungredda95@gmail.com>
2022-11-23 16:51:26 -0400

    Fix linters

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -12,34 +12,34 @@ RSpec.describe Post, type: :feature do
     @comment2 = Comment.create(post: @post2, user: @user1, text: 'Hi Samuel!')
   end
 
-  describe 'post/show' do
+  describe "post/show" do
     include RSpecHtmlMatchers
 
     before(:each) do
       visit "users/#{@user1.id}/posts/#{@user1.posts[0].id}"
     end
 
-    it 'show who wrote the post.' do
+    it "show who wrote the post." do
       expect(page).to have_content(@post1.user.name)
     end
 
-    it 'show how many comments it has.' do
+    it "show how many comments it has." do
       expect(page).to have_content(@post1.comments_counter)
     end
 
-    it 'show how many likes it has.' do
+    it "show how many likes it has." do
       expect(page).to have_content(@post1.likes_counter)
     end
 
-    it 'show the post body.' do
+    it "show the post body." do
       expect(page).to have_content(@post1.text)
     end
 
-    it 'show the username of each commentor.' do
+    it "show the username of each commentor." do
       expect(page).to have_content(@comment1.user.name)
     end
 
-    it 'show the comment each commentor left.' do
+    it "show the comment each commentor left." do
       expect(page).to have_content(@comment1.text)
     end
   end
--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -8,22 +8,22 @@ RSpec.describe User, type: :feature do
     Post.create(user: @user1, title: 'Hill', text: 'Creating post')
   end
 
-  describe 'user show view' do
+  describe "user show view" do
     RSpec.configure do |config|
       config.include RSpecHtmlMatchers
     end
 
-    it 'show usernames' do
+    it "show usernames" do
       visit "/users/#{@user1.id}"
       expect(page).to have_content(@user1.name)
     end
 
-    it 'show total number of post created' do
+    it "show total number of post created" do
       visit "/users/#{@user1.id}"
       expect(page).to have_content(@user1.posts_counter)
     end
 
-    it 'show each user bio' do
+    it "show each user bio" do
       visit "/users/#{@user1.id}"
       expect(page).to have_content(@user1.bio)
     end
@@ -41,12 +41,12 @@ RSpec.describe User, type: :feature do
       expect(page).to have_tag('button')
     end
 
-    it 'when you click to an user, redirects to post index page' do
+    it "when you click to an user, redirects to post index page" do
       visit "/users/#{@user1.id}"
       expect(page).to have_tag('a', href: user_posts_path(@user1))
     end
 
-    it 'when you click to an user, redirects to post show page' do
+    it "when you click to an user, redirects to post show page" do
       visit "/users/#{@user1.id}"
       expect(page).to have_tag('a', href: user_post_path(@user1, @user1.posts[0].id))
     end
